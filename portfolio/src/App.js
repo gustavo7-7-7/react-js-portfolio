f4b68ffd853ad66b4efd3eaaab29c766761b50c2
gustavo7-7-7 <gustavohr.rossi@gmail.com>
2022-10-27 09:52:15 -0300

    feat Home

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import './App.css';
-import Header from './components/header/Header';
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+
 
 const App = () => {
   return (
     <>
-    <Header/>
+      <Header />
+      <main className="main">
+        <Home />
+        <About/>
+        <Skills/>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
